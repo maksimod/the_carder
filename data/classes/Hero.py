569943f5cf311a33bb04c34cff2598664806f1b5
Maksim Koptelov <maksumonka@gmail.com>
2024-07-18 21:20:00 +0300

    Add hero mp img

--- data/classes/Hero.py
+++ data/classes/Hero.py
@@ -4,6 +4,7 @@ from data.global_vars import hero
 from data.global_vars.screen_info import *
 
 from data.classes.constructor.Lines_constructor import Line
+from data.classes.constructor.Elements import Img, CText
 
 # heroes
 hero_scale = 0.3
@@ -39,6 +40,8 @@ class Hero:
 
         self.hero_debuffs = hero.hero[hero.hero_class][0][-1]
         self.deb_index = hero.debuffs_indexes
+        
+        self.hero_mp_img = Img('data/images/elements/mp/bercerk_mp.png', k=0.4*screen_scale)
 
     def make_turn(self):
         # self.current_mp = self.max_mp
@@ -59,11 +62,13 @@ class Hero:
             str(self.hero_debuffs[self.deb_index['poison']]) + ' POISON', False,
             (255, 255, 255))
         screen.blit(hero_poison_text_surface, (0, hero_poison_text_surface.get_height() * 1))
-        hero_mp_text_surface = self.hero_mp_text.render(
-            str(self.hero_hp_mp[3]) + '/' + str(self.hero_max_hp_mp[1]) + ' MP', False, (255, 255, 255))
-        screen.blit(hero_mp_text_surface, (0, hero_mp_text_surface.get_height() * 3))
 
     def update_hero(self):
         screen.blit(self.player_surface, hero_position)
         self.draw_hero_text()
         self.hero_line.draw(hero.hero[self.hero_class])
+        self.hero_mp_img.draw((65*screen_scale,420*screen_scale))
+        self.current_mp_text = CText(str(self.hero_hp_mp[3])+'/', k=0.3*screen_scale)
+        self.max_mp_text = CText(str(self.hero_max_hp_mp[1]), k=0.3 * screen_scale)
+        self.current_mp_text.draw((85*screen_scale,450*screen_scale))
+        self.max_mp_text.draw((110 * screen_scale, 450 * screen_scale))
